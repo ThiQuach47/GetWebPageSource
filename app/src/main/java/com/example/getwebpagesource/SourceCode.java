734b7c82bc18c5c0fc516ccd1e8669a68708a180
thiquach47 <thiquach910@gmail.com>
2022-04-26 13:12:14 +0700

    GetWebPageSource-19110294

---
+++ app/src/main/java/com/example/getwebpagesource/SourceCode.java
@@ -0,0 +1,34 @@
+package com.example.getwebpagesource;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
+
+public class SourceCode extends AsyncTaskLoader<String> {
+
+    private String queryString;
+    private String transferProtocol;
+    private Context context;
+
+    public SourceCode(String queryString, String transferProtocol, Context context) {
+        super(context);
+        this.queryString = queryString;
+        this.transferProtocol = transferProtocol;
+        this.context = context;
+    }
+
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return null;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+
+}
